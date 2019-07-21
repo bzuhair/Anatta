77e983f799ff1573a2a46170c5a6860707db3cb4
Balal Zuhair <bzuhair3@gmail.com>
2019-07-21 15:34:52 -0700

    first commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  
+  render() {
+    return (
+      <div id="main">
+        Main
+      </div>
+    )
+  }
+}
+
+
+export default Main;
