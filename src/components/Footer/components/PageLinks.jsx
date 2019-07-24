ce2032c70b299d95ac78c51753096fb0b96d455a
Balal Zuhair <bzuhair3@gmail.com>
2019-07-23 20:44:19 -0700

    remove console and add key

--- src/components/Footer/components/PageLinks.jsx
+++ src/components/Footer/components/PageLinks.jsx
@@ -4,12 +4,11 @@ import pages from '../../../constants/pages';
 
 
 const PageLinks = () => {
-  const sections = Object.keys(pages).map((section) => {
-    console.log(section);
+  const sections = Object.keys(pages).map((section, idx) => {
     let links = [];
-    pages[section].forEach((page) => links.push(<a href={page.link} className="page">{page.name}</a>))
+    pages[section].forEach((page) => links.push(<a key={`links${page.name}-idx`} href={page.link} className="page">{page.name}</a>))
     return (
-      <div className="section">
+      <div key={`${section}-idx`} className="section">
         <div className="header">{section}</div>
         <div className="content">
           {links}
