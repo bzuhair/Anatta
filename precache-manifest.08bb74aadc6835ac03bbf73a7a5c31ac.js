4d1339ed6d4ab3543aaa06205bc606e07833e366
Balal Zuhair <bzuhair3@gmail.com>
2019-07-25 12:51:58 -0700

    Updates

---
+++ precache-manifest.08bb74aadc6835ac03bbf73a7a5c31ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8626c809e665c048bf8ae090bf1b1e42",
+    "url": "/anatta/index.html"
+  },
+  {
+    "revision": "186d70ef510c438d64dd",
+    "url": "/anatta/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "a11199144fc469ab7b4b",
+    "url": "/anatta/static/css/main.e4121a69.chunk.css"
+  },
+  {
+    "revision": "186d70ef510c438d64dd",
+    "url": "/anatta/static/js/2.8ca5f64b.chunk.js"
+  },
+  {
+    "revision": "a11199144fc469ab7b4b",
+    "url": "/anatta/static/js/main.45d771b1.chunk.js"
+  },
+  {
+    "revision": "b88f169e240a6945cc1c",
+    "url": "/anatta/static/js/runtime~main.442fbcf2.js"
+  }
+]);
