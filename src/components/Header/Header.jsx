db6875f76dd3190b8f1cbfbdd0e2e7f9d30c1ea1
Balal Zuhair <bzuhair3@gmail.com>
2019-07-27 16:09:56 -0700

    refactor Header to be a functional component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import CheckoutIcon from '../../svgs/CheckoutIcon';
 import AccountIcon from '../../svgs/AccountIcon';
 
-export class Header extends Component {
-  constructor() {
-    super();
-    this.state = {}
-  }
-  render() {
-    return (
-      <div className="header-container">
-        <div className='top center-content-flex'><div>FREE US SHIPPING & RETURN</div></div>
-        <div className="nav-bar">
-          <div className="header-left">
-            <span style={{ marginRight: '40px' }}>Men</span>
-            <span>Women</span>
-          </div>
-          <div className="logo-wrapper">
-            <div className="logo">
-              <span className="oval-yellow" />
-              <span className="oval-green" />
-            </div>
-          </div>
-          <div className="header-right">
-            <span style={{ marginRight: '38px' }}>About</span>
-            <div className='icons'>
-              <AccountIcon />
-              <CheckoutIcon />
-            </div>
-          </div>
+const Header = () => (
+  <div className="header-container">
+    <div className='top center-content-flex'><div>FREE US SHIPPING & RETURN</div></div>
+    <div className="nav-bar">
+      <div className="header-left">
+        <span style={{ marginRight: '40px' }}>Men</span>
+        <span>Women</span>
+      </div>
+      <div className="logo-wrapper">
+        <div className="logo">
+          <span className="oval-yellow" />
+          <span className="oval-green" />
         </div>
       </div>
-    )
-  }
-}
+      <div className="header-right">
+        <span style={{ marginRight: '38px' }}>About</span>
+        <div className='icons'>
+          <AccountIcon />
+          <CheckoutIcon />
+        </div>
+      </div>
+    </div>
+  </div>
+);
 
 export default Header;
